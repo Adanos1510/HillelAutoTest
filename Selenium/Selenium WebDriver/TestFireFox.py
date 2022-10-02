e13ef79cbb9ee11303a985ab1c413efbe7c09265
Vlad <adanos1510@gmail.com>
2022-10-02 20:37:19 +0300

    selenium

---
+++ Selenium/Selenium WebDriver/TestFireFox.py
@@ -0,0 +1,32 @@
+#from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+options = Options()
+
+driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
+
+user = "guest"
+password = "welcome2qauto"
+driver.get("https://"+user+":"+password+"@"+"qauto2.forstudy.space/")
+
+SignInButton = driver.find_element(By.XPATH, "//button [@class='btn btn-outline-white header_signin']")
+SignInButton.click()
+Email = driver.find_element(By.XPATH, "(//input)[1]")
+Email.send_keys("Pupsiki2group@gmail.com")
+Password = driver.find_element(By.XPATH, "(//input)[2]")
+Password.send_keys("Grupadva123")
+remember = driver.find_element(By.NAME, "remember")
+remember.click()
+LoginButton = driver.find_element(By.XPATH, "/html/body/ngb-modal-window/div/div/app-signin-modal/div[3]/button[2]")
+LoginButton.send_keys(Keys.RETURN)
+
+assert "Hillel auto developed in Hillel IT school for educational purposes of QA courses." in driver.page_source
+time.sleep(2)
+
+driver.close()
